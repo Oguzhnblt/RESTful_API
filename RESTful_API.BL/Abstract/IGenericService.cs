12c36c38e7f68863e96d13da1e83083f1601407c
Oğuzhan Bolat <oguzhanbolat0@gmail.com>
2023-02-20 13:18:16 +0300

    Added Business Layer

---
+++ RESTful_API.BL/Abstract/IGenericService.cs
@@ -0,0 +1,12 @@
+﻿namespace RESTful_API.BL.Abstract
+{
+    public interface IGenericService<T>
+    {
+
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> Add(T item);
+        Task<T> Update(T item);
+        Task<T> Delete(T item);
+    }
+}
---
+++ RESTful_API.BL/Concrate/GenericManager.cs
@@ -0,0 +1,40 @@
+﻿using RESTful_API.BL.Abstract;
+using RESTful_API.DAL.Repository.Abstract;
+
+namespace RESTful_API.BL.Concrate
+{
+    public class GenericManager<T> : IGenericService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        public GenericManager(IGenericRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<T> Add(T item)
+        {
+            return _repository.Add(item);
+        }
+
+        public Task<T> Delete(T item)
+        {
+            return _repository.Delete(item);
+        }
+        public Task<IEnumerable<T>> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public Task<T> Update(T item)
+        {
+
+            return _repository.Update(item);
+
+        }
+    }
+}
