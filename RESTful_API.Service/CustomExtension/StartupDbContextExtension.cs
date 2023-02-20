f97e9529738eef869c8ccbbd0ae8e316fdd030cc
Oğuzhan Bolat <oguzhanbolat0@gmail.com>
2023-02-20 13:54:37 +0300

    Added DbContext options

---
+++ RESTful_API.Service/CustomExtension/StartupDbContextExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RESTful_API.DAL.Context;
+
+namespace RESTful_API.Service.CustomExtension
+{
+    public static class StartupDbContextExtension
+    {
+        public static void AddDbContextDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            var dbtype = configuration.GetConnectionString("DbType");
+            if (dbtype == "SQL")
+            {
+                var dbConfig = configuration.GetConnectionString("DefaultConnection");
+                services.AddDbContext<RESTful_Api_Context>(options => options
+                   .UseSqlServer(dbConfig)
+                   );
+            }
+            else if (dbtype == "PostgreSQL")
+            {
+                var dbConfig = configuration.GetConnectionString("PostgreSqlConnection");
+                services.AddDbContext<RESTful_Api_Context>(options => options
+                   .UseNpgsql(dbConfig)
+                   );
+            }
+        }
+    }
+}
